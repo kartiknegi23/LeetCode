dfc7590d3a571f9bd53a56d1118fc09bc287b940
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-03-28 01:10:13 +0530

    Time: 60 ms (94.43%) | Memory: 57.9 MB (62.03%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.js
+++ 41-first-missing-positive/first-missing-positive.js
@@ -4,19 +4,18 @@
  */
 var firstMissingPositive = function(nums) {
     
+
     for(let i=0;i<nums.length;i++){
-        while(nums[i] > 0 && nums[i] <= nums.length && nums[nums[i]-1]!==nums[i]){
-            let temp = nums[i];
-            nums[i] = nums[nums[i]-1]; 
-            nums[temp-1] = temp;
+
+        while(nums[i] > 0 && nums[i] <=nums.length && nums[nums[i]-1]!==nums[i]){
+            [nums[nums[i]-1], nums[i]] = [nums[i], nums[nums[i]-1]];
         }
     }
 
     for(let i=0;i<nums.length;i++){
-        if(i!=nums[i]-1)
+        if(nums[i]-1!==i)
         return i+1;
     }
 
     return nums.length+1;
-
 };
