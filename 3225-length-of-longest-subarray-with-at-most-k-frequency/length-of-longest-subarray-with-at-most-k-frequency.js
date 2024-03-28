cb47930fbb7d7335ea7356b07e9e02e68be7d410
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-03-29 00:42:03 +0530

    Time: 1580 ms (5.31%) | Memory: 71.9 MB (8.85%) - LeetSync

---
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var maxSubarrayLength = function(nums, k) {
+    
+    let hashmap = new Map();
+    let left = 0;
+    let count = 0;
+
+    for(let right = 0; right<nums.length; right++){
+        if(hashmap.has(nums[right]))
+        hashmap.set(nums[right], hashmap.get(nums[right])+1);
+
+        else
+        hashmap.set(nums[right], 1);
+
+        while(hashmap.get(nums[right]) > k){
+            console.log(count);
+            hashmap.set(nums[left], hashmap.get(nums[left])-1);
+            left++;
+        }
+
+        count = Math.max(count, right - left + 1);
+
+    }
+
+    return count;
+};
