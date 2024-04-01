a6a82102d7df8dfe35dad903e3a38994f272d0ff
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-04-01 13:09:52 +0530

    Time: 65 ms (5.18%) | Memory: 49 MB (26.81%) - LeetSync

---
+++ 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe : function (value){
+            if(val===value)
+            return true;
+            else
+            throw Error("Not Equal");
+        },
+        notToBe : function (value){
+            if(val!==value)
+            return true;
+            else
+            throw Error("Equal");
+        }
+    }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
