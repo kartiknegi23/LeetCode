0f7a7c82f9fdc8faf021259ec4b2500e6f0769af
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-04-02 15:02:21 +0530

    Time: 54 ms (58.53%) | Memory: 51 MB (9.57%) - LeetSync

--- 1585-the-kth-factor-of-n/the-kth-factor-of-n.js
+++ 1585-the-kth-factor-of-n/the-kth-factor-of-n.js
@@ -5,10 +5,11 @@
  */
 var kthFactor = function(n, k) {
     arr = [];
-    for(let i=1;i<=n;i++){
+    for(let i=1;i<=n/2;i++){
         if(n%i===0)
         arr.push(i);
     }
+    arr.push(n);
     if(arr.length < k)
     return -1;
 
