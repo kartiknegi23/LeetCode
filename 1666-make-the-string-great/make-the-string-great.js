889b335f4ccdd0b425d8dffd97419ad758c07df3
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-04-05 13:53:23 +0530

    Time: 50 ms (91.22%) | Memory: 51.5 MB (29.28%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.js
+++ 1666-make-the-string-great/make-the-string-great.js
@@ -12,12 +12,8 @@ var makeGood = function(s) {
         else
         stack.push(s[i]);
 
-        console.log(stack);
 
     }
-
-    // if(s[s.length-1]!==stack[stack.length-1] && stack[stack.length-1]!==s[s.length-1].toLowerCase() && stack[stack.length-1]!==s[s.length-1].toUpperCase())
-    // stack.push(s[s.length-1]);
 
     for(let j=0;j<stack.length;j++){
         str+=stack[j];
