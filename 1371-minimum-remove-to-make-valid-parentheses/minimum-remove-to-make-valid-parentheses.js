fac8ca7128d09cee2b291ed74adbb231f1023175
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-04-06 22:53:52 +0530

    Time: 170 ms (5.42%) | Memory: 57.6 MB (45.86%) - LeetSync

---
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var minRemoveToMakeValid = function(s) {
+    let str = "";
+    let stack = [];
+    let index = [];
+    let count1 = 0;
+    let count2 = 0;
+
+    for(let i=0;i<s.length;i++){
+        if(s[i]=="("){
+            stack.push(s[i]);
+            index.push(i);
+        }
+
+        if(s[i]==")"){
+            if(stack.length==0){
+                index.push(i);
+            }
+            else{
+                stack.pop();
+                index.pop();
+            }
+        }
+
+    }
+
+    for(let i=0;i<s.length;i++){
+        if(index.includes(i))
+        continue;
+
+        str+=s[i];
+    }
+    return str;
+};
