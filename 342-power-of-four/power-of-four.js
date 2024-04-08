d33cfb1329353807280aea72d9ddf68809b8eb03
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-04-09 01:48:15 +0530

    Time: 68 ms (73.16%) | Memory: 52.8 MB (28.46%) - LeetSync

--- 342-power-of-four/power-of-four.js
+++ 342-power-of-four/power-of-four.js
@@ -6,8 +6,13 @@ var isPowerOfFour = function(n) {
     if(n<=0)
     return false;
 
-    if(n==1)
+    while(n%4===0){
+        n/=4;
+    }
+
+    if(n===1)
     return true;
-    
-    return isPowerOfFour(n/4);
+
+    else
+    return false;
 };
