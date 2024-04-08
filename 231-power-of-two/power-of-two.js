3270281c1aedc0c37179c6a3d1aa35909e098fde
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-04-08 17:18:11 +0530

    Time: 93 ms (5.19%) | Memory: 52.8 MB (76.19%) - LeetSync

--- 231-power-of-two/power-of-two.js
+++ 231-power-of-two/power-of-two.js
@@ -6,6 +6,6 @@ var isPowerOfTwo = function(n) {
     if(n<=0)
     return false
 
-    return (Math.floor(Math.log2(n)/Math.log2(2)) === Math.ceil(Math.log2(n)/Math.log2(2)))
+    return Math.floor(Math.log2(n)) === Math.ceil(Math.log2(n));
 
 };
