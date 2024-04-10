5b7f114d5d94ea5782158f545b8b29f6cd96b357
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-04-11 01:04:55 +0530

    Time: 144 ms (89.96%) | Memory: 70.1 MB (37.27%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.js
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.js
@@ -4,22 +4,26 @@
  * @return {number}
  */
 var maxSubarrayLength = function(nums, k) {
+    let count = 0;
     let hashmap = new Map();
     let left = 0;
-    let count = 0;
 
-    for(let right =0; right<nums.length; right++){
-        if(hashmap.has(nums[right]))
-        hashmap.set(nums[right], hashmap.get(nums[right])+1);
+    for(let right=0;right<nums.length;right++){
+        if(hashmap.has(nums[right])){
+            hashmap.set(nums[right], hashmap.get(nums[right])+1);
+        }
 
         else
-        hashmap.set(nums[right], 1);
-
+        {
+            hashmap.set(nums[right], 1);
+        }
+        
         while(hashmap.get(nums[right]) > k){
             hashmap.set(nums[left], hashmap.get(nums[left])-1);
             left++;
         }
-        count = Math.max(count, right - left + 1);
+
+        count = Math.max(count, right-left+1);
     }
 
     return count;
