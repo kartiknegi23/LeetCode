2d12d87fe0e971361a1a087be1fb1f6a106623a4
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-04-19 02:16:06 +0530

    Time: 105 ms (5.08%) | Memory: 53.5 MB (6.13%) - LeetSync

--- 404-sum-of-left-leaves/sum-of-left-leaves.js
+++ 404-sum-of-left-leaves/sum-of-left-leaves.js
@@ -11,27 +11,27 @@
  * @return {number}
  */
 var sumOfLeftLeaves = function(root) {
-    // Initialize the sum
+    if(root===null)
+    return 0;
+
+    let arr = [];
+
     let sum = 0;
-    
-    // Define a recursive function for depth-first traversal
-    const dfs = (node, isLeft) => {
-        // If the node is null, return
-        if (!node) return;
+    let isleft = false;
+    function inorder(root, isleft){
+        if(root===null)
+        return;
+
+        if(root.left===null && root.right===null && isleft===true)
+        sum+=root.val;
+
+        console.log(root.val);
+
+        inorder(root.left,true);
         
-        // If the node is a leaf node and a left child, add its value to the sum
-        if (!node.left && !node.right && isLeft) {
-            sum += node.val;
-        }
-        
-        // Recursively traverse the left and right subtrees
-        dfs(node.left, true);   // For the left child, update isLeft to true
-        dfs(node.right, false); // For the right child, isLeft remains false
-    };
+        inorder(root.right,false);
+    }
     
-    // Start the depth-first traversal from the root with isLeft set to false
-    dfs(root, false);
-    
-    // Return the sum of left leaves
+    inorder(root, false);
     return sum;
 };
