60d05c0ac0b148c714dcd2e5c17039eac1d5ef3a
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-04-21 03:11:05 +0530

    Time: 138 ms (37.05%) | Memory: 58.2 MB (49.11%) - LeetSync

--- 463-island-perimeter/island-perimeter.js
+++ 463-island-perimeter/island-perimeter.js
@@ -3,30 +3,26 @@
  * @return {number}
  */
 var islandPerimeter = function(grid) {
-    
     let row = grid.length;
     let col = grid[0].length;
     let perimeter = 0;
 
+    function dfs(r, c){
+        if(r<0 || r>=row || c<0 || c>=col || grid[r][c]===0)
+        return 1;
+
+        if(grid[r][c]===-1)
+        return 0;
+
+        grid[r][c]=-1;
+
+        return (dfs(r-1,c)+dfs(r+1,c)+dfs(r,c-1)+dfs(r,c+1));
+    }
+
     for(let i=0;i<row;i++){
         for(let j=0;j<col;j++){
             if(grid[i][j]===1)
-            {   
-                perimeter+=4;
-
-            if(j > 0)
-                    {
-                        if(grid[i][j-1]===1)
-                        perimeter-=2;
-                    }
-
-            if(i > 0)
-                    {
-                        if(grid[i-1][j]===1)
-                        perimeter-=2;
-                    } 
-            }
-            
+            perimeter+=dfs(i,j);
         }
     }
 
