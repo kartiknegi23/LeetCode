6fbb92101bcb9824e8ad83f9872b250c643657e7
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-04-21 20:59:02 +0530

    Time: 117 ms (14.99%) | Memory: 58.8 MB (29.05%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.js
@@ -0,0 +1,34 @@
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var numIslands = function(grid) {
+    let count = 0;
+    let row = grid.length;
+    let col = grid[0].length;
+
+    function dfs(r,c){
+        if(r<0 || r>=row || c<0 || c>=col || grid[r][c]==="0" || grid[r][c]===-1)
+        return;
+
+        grid[r][c]=-1;
+
+        dfs(r-1,c);
+        dfs(r+1,c);
+        dfs(r,c-1);
+        dfs(r,c+1);
+    }
+
+    for(let i=0;i<row;i++){
+        for(let j=0;j<col;j++){
+            if(grid[i][j]==="1")
+            {
+                console.log("hello", i, j);
+                count++;
+                dfs(i,j);
+            }
+        }
+    }
+
+    return count;
+};
