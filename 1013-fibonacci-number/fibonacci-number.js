321121bc72e60045357b7d207ca1dfb69253a2e1
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-04-24 14:05:32 +0530

    Time: 43 ms (94.43%) | Memory: 48.6 MB (65.17%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.js
+++ 1013-fibonacci-number/fibonacci-number.js
@@ -3,8 +3,12 @@
  * @return {number}
  */
 var fib = function(n) {
-    if(n<=1)
-    return n;
+    let arr = [];
+    arr[0] = 0;
+    arr[1] = 1;
 
-    return fib(n-1)+fib(n-2);
+    for(let i=2;i<=n;i++){
+        arr[i] = arr[i-1]+arr[i-2];
+    }  
+    return arr[n];
 };
