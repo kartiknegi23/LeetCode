e45e78acbc56f359a401ec37aa2216dcbb79bbf9
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-04-24 20:06:11 +0530

    Time: 55 ms (45.10%) | Memory: 49.3 MB (18.05%) - LeetSync

--- 75-sort-colors/sort-colors.js
+++ 75-sort-colors/sort-colors.js
@@ -2,14 +2,45 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var sortColors = function(nums) {
-    for(let i=0;i<nums.length-1;i++){
-        for(let j=i+1;j<nums.length;j++){
-            if(nums[i]>nums[j]){
-                let temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
-            }
+function quicksort(nums,low,high){
+    if(low<high){
+        let pivot_index = find_index(nums,low,high);
+
+        quicksort(nums,low,pivot_index-1);
+        quicksort(nums,pivot_index+1,high);
+    }
+}
+
+function find_index(nums,low,high){
+    let i = low;
+    let j= high;
+
+    let pivot = nums[low];
+
+    while(i<j){
+        while(nums[i]<=pivot && i<=high-1)
+        i++;
+
+        while(nums[j]>pivot && j>=low+1)
+        j--;
+
+        if(i<j)
+        {
+            let temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
         }
     }
+
+    let temp = nums[low];
+    nums[low] = nums[j];
+    nums[j] = temp;
+    
+    return j;
+}
+
+
+var sortColors = function(nums) {
+    
+    quicksort(nums,0,nums.length-1);
 };
