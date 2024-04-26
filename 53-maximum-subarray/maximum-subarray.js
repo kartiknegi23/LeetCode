44f0c28a6f43f6d0ba03cc8d6e541ebba90a05f8
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-04-27 00:16:43 +0530

    Time: 74 ms (46.54%) | Memory: 57.4 MB (69.87%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.js
+++ 53-maximum-subarray/maximum-subarray.js
@@ -4,33 +4,16 @@
  */
 var maxSubArray = function(nums) {
     let sum = 0;
-    let maxi = -Infinity;
-    let sub = [];
-    let start = -1;
-    let end = -1;
+    let max = -Infinity;
 
     for(let i=0;i<nums.length;i++){
-        if(sum===0){
-            start = i;
-        }
+        sum += nums[i];
 
-
-        sum+=nums[i];
-
-        if(maxi<sum)
-        {
-            maxi = sum;
-            end = i;
-        }
+        if(sum>max)
+        max = sum;
 
         if(sum<0)
         sum=0;
     }
-
-    for(let i=start;i<=end;i++){
-        sub.push(nums[i]);
-    }
-
-    console.log(sub);
-    return maxi;
+    return max;
 };
