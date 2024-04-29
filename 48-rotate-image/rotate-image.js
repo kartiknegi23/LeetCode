8df10f572c05cacc08caaf664e0d9c8c19985255
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-04-30 03:13:39 +0530

    Time: 58 ms (27.14%) | Memory: 51.5 MB (12.59%) - LeetSync

--- 48-rotate-image/rotate-image.js
+++ 48-rotate-image/rotate-image.js
@@ -5,7 +5,7 @@
 var rotate = function(matrix) {
     let n = matrix.length;
 
-    for(let i=0;i<n;i++){
+    for(let i=0;i<n-1;i++){
         for(let j=i+1;j<n;j++){
             let temp = matrix[i][j];
             matrix[i][j]=matrix[j][i];
