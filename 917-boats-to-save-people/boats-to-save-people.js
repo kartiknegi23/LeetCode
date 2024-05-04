d607ec40d47c87059b10acd801c3ae87dca29fb4
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-05-05 00:32:52 +0530

    Time: 128 ms (63.19%) | Memory: 56.9 MB (71.30%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.js
+++ 917-boats-to-save-people/boats-to-save-people.js
@@ -5,22 +5,16 @@
  */
 var numRescueBoats = function(people, limit) {
     people.sort((a,b)=>a-b);
-
-    
     let count = 0;
     let left = 0;
-    let right = people.length-1; 
+    let right = people.length-1;
 
-    for(let i=0;i<people.length;i++){
-            
-        while(left<=right){
-            count++;
-            if(people[left]+people[right]<=limit)
-            {
-                left++;
-            }
-            right--;
+    while(left<=right){
+        count++;
+        if(people[left]+people[right]<=limit){
+            left++;
         }
+        right--;
     }
     return count;
 };
