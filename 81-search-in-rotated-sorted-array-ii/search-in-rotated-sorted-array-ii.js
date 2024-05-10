c42bec14c2a67f6b7082098334864cdb608955db
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-05-10 17:39:05 +0530

    Time: 62 ms (17.69%) | Memory: 49.1 MB (71.05%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.js
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.js
@@ -4,47 +4,39 @@
  * @return {boolean}
  */
 var search = function(nums, target) {
-    let flag = false;
 
     let left = 0;
     let right = nums.length-1;
 
     while(left<=right){
-
-        
-
         let mid = Math.floor((left+right)/2);
 
-        
-
-        if(target===nums[mid]){
-            return true;
+        if(nums[mid]===target){
+            return  true;
         }
 
-
-        if(nums[left]===nums[mid] && nums[mid]===nums[right]){
+        if(nums[mid] === nums[left] && nums[mid]===nums[right]){
             left++;
             right--;
             continue;
         }
-        
 
         else if(nums[left]<=nums[mid]){
-            if(nums[left]<=target && target<=nums[mid])
-            right=mid-1;
-
+            if(nums[left]<=target && target<=nums[mid]){
+                right=mid-1;
+            }
             else
             left=mid+1;
         }
 
         else{
-            if(nums[mid]<= target && target<=nums[right])
-            left=mid+1;
-
+            if(nums[mid]<= target && target <= nums[right]){
+                left=mid+1;
+            }
             else
             right=mid-1;
         }
     }
 
-    return flag;
+    return false;
 };
