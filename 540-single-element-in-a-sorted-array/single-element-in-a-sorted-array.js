0d957dd9e5442014edbf2e1349b976325677b4a7
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-05-10 22:02:16 +0530

    Time: 58 ms (34.45%) | Memory: 51.8 MB (52.81%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.js
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.js
@@ -3,12 +3,12 @@
  * @return {number}
  */
 var singleNonDuplicate = function(nums) {
-    let left = 0;
+    let left =0;
     let right = nums.length-1;
 
     while(left<=right){
         let mid = Math.floor((left+right)/2);
-
+        
         if(nums[mid]!==nums[mid-1] && nums[mid]!==nums[mid+1]){
             return nums[mid];
         }
@@ -23,11 +23,10 @@ var singleNonDuplicate = function(nums) {
 
         else{
             if(mid%2===0)
-            left=mid+1;
+            left = mid+1;
 
             else
-            right=mid-1;
+            right = mid-1; 
         }
     }
-
 };
