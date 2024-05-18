0782c37eb0744c6953f68b643fcb42259e1d192e
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-05-19 01:02:23 +0530

    Time: 119 ms (7.32%) | Memory: 58.3 MB (7.32%) - LeetSync

---
+++ 1021-distribute-coins-in-binary-tree/distribute-coins-in-binary-tree.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+
+function dfs(node, context) {
+    if (node === null)
+        return [0, 0];
+
+    let left = dfs(node.left, context);
+    let right = dfs(node.right, context);
+
+    let n = left[0] + right[0] + 1;
+    let coins = left[1] + right[1] + node.val;
+
+    context.ans += Math.abs(coins - n);
+    console.log(coins, n, context.ans);
+
+    return [n, coins];
+}
+
+var distributeCoins = function (root) {
+    const context = { ans: 0 };  /// CLOSURE ///
+    dfs(root, context);
+    return context.ans;
+};
