a980e7980f76c9c52f76aa51295e1bf050cf58aa
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-05-24 21:19:23 +0530

    Time: 290 ms (5.11%) | Memory: 57.9 MB (5.11%) - LeetSync

---
+++ 410-split-array-largest-sum/split-array-largest-sum.js
@@ -0,0 +1,54 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+
+function check(nums,mid){
+    let count = 1;
+    let sum = 0;
+    for(let i=0;i<nums.length;i++){
+        sum+=nums[i];
+        console.log(sum,mid);
+        if(sum>mid){
+            count++;
+            sum=nums[i];
+        }
+    }
+    console.log("count:",count)
+    return count;
+}
+
+var splitArray = function(nums, k) {
+    let sum = 0;
+    let min = -Infinity;
+    let ans = 0;
+
+    if(k>nums.length)
+    return -1;
+
+    for(let i=0;i<nums.length;i++){
+        sum+=nums[i];
+
+        min = min<nums[i] ? nums[i] : min;
+    }
+
+    let low = min;
+    let high = sum;
+
+    while(low<=high){
+        let mid = Math.floor((low+high)/2);
+
+        let count = check(nums,mid);
+
+        if(count<=k){
+            ans = mid;
+            high=mid-1;
+        }
+
+        else
+        low=mid+1;
+    }
+
+    return ans;
+};
