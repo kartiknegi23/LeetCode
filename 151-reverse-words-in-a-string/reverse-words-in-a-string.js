852a7804721f8d5d15a7deafe8720014fc3570e8
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-05-30 02:57:10 +0530

    Time: 70 ms (7.25%) | Memory: 51.6 MB (13.55%) - LeetSync

---
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+    let arr = s.split(" ");
+    arr = arr.reverse();
+    let ans = "";
+
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]==="")
+        {arr.splice(i,1);
+        i--;}
+    }
+
+    console.log(arr);
+
+    for(let i=0;i<arr.length;i++){
+        ans+=arr[i];
+        if(i<arr.length-1)
+        ans+=" ";
+    }
+
+    return ans;
+};
