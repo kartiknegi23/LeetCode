57a1bf5787ea837d312f9a53de07b758c7e743e4
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-06-01 12:12:04 +0530

    Time: 67 ms (28.58%) | Memory: 50 MB (52.03%) - LeetSync

--- 3379-score-of-a-string/score-of-a-string.js
+++ 3379-score-of-a-string/score-of-a-string.js
@@ -4,9 +4,9 @@
  */
 var scoreOfString = function(s) {
     let score = 0;
-    
     for(let i=0;i<s.length-1;i++){
-        score+=Math.abs(s.charCodeAt(i)-s.charCodeAt(i+1));
+        score+= Math.abs(s.charCodeAt(i)-s.charCodeAt(i+1));
     }
+
     return score;
 };
