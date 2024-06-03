cf3046d0f25205fb37d321f237a903a8830d6aec
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-06-03 15:47:53 +0530

    Time: 67 ms (34.01%) | Memory: 51.3 MB (72.53%) - LeetSync

---
+++ 61-rotate-list/rotate-list.js
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+var rotateRight = function(head, k) {
+
+    if(head===null)
+    return head;
+
+    if(head.next==null)
+    return head;
+
+    let length=0;
+    let temp = head;
+
+    while(temp!==null){
+        length++;
+        temp=temp.next;
+    }
+
+    k = k%length;
+
+    let count = 0;
+
+    while(count<k){
+        let current = head;
+        let prev = null;
+        while(current.next!==null){
+            prev = current;
+            current = current.next;
+        }
+        current.next = head;
+        prev.next = null;
+        head = current;
+
+        count++;
+    }
+    return head;
+};
