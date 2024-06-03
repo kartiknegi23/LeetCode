b0824f1ad01aa8f479644478371c0a9dd6a4557c
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-06-03 10:28:06 +0530

    Time: 72 ms (5.75%) | Memory: 52.2 MB (78.16%) - LeetSync

---
+++ 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {number}
+ */
+var appendCharacters = function(s, t) {
+    let i=0;
+    let j=0;
+
+    while(i<s.length){
+        if(s[i]===t[j])
+        j++;
+        i++;
+    }
+
+    return t.length-j;
+};
