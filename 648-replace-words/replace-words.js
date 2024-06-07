ddf0652a4f88f5e51a3f3bd24c972edd5a6601ea
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-06-08 03:04:57 +0530

    Time: 2649 ms (5.40%) | Memory: 62.7 MB (64.32%) - LeetSync

---
+++ 648-replace-words/replace-words.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string[]} dictionary
+ * @param {string} sentence
+ * @return {string}
+ */
+
+function findroot(arr, dictionary){
+    let temp = "";
+    for(let i=0;i<arr.length;i++){
+        temp += arr[i];
+        for(let j=0;j<dictionary.length;j++){
+            if(temp===dictionary[j]){
+                return temp;
+            }            
+        }
+    }
+    return arr;
+}
+
+var replaceWords = function(dictionary, sentence) {
+    let str = "";
+    let arr = sentence.split(" ");
+
+    for(let i=0;i<arr.length;i++){
+        str += findroot(arr[i], dictionary)+" ";
+    }
+    
+    ans = str.slice(0,-1);
+
+    return ans;
+};
