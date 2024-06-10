0d9463c8e536087a0f021f8f5e698ddee9d16570
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-06-11 03:09:23 +0530

    Time: 62 ms (58.29%) | Memory: 52.6 MB (15.42%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.js
+++ 496-next-greater-element-i/next-greater-element-i.js
@@ -4,22 +4,42 @@
  * @return {number[]}
  */
 var nextGreaterElement = function(nums1, nums2) {
-    let arr = [];
+    let stack = [];
+    let arr = new Array(nums2.length);
+    let ans = [];
+    let hashmap = new Map();
 
-    for(let i=0;i<nums1.length;i++){
-        let ele = nums1[i];
-        let index = nums2.indexOf(ele);
-        let max = ele;
-        for(let j=index+1;j<nums2.length;j++){
-            max = Math.max(max, nums2[j]);
-            if(max>ele){
-                arr.push(max);
-                break;
-            }
+    for(let i=nums2.length-1;i>=0;i--){
+        if(stack.length===0)
+        {
+            stack.push(nums2[i]);
+            arr[i] = -1;
+            hashmap.set(nums2[i], arr[i]);
+            continue;
         }
-        if(max===ele)
-        arr.push(-1);
+
+        while(nums2[i] > stack[stack.length-1] && stack.length>0){
+            stack.pop();
+        }
+
+        if(stack.length===0)
+        {
+            arr[i] = -1;
+            stack.push(nums2[i]);
+            hashmap.set(nums2[i], arr[i]);
+            continue;
+        }
+
+        arr[i] = stack[stack.length-1];
+        hashmap.set(nums2[i], arr[i]);
+        stack.push(nums2[i]);
+        
+        
     }
 
-    return arr;
+    for(let i=0;i<nums1.length;i++){
+        ans.push(hashmap.get(nums1[i]));
+    }
+
+    return ans;
 };
