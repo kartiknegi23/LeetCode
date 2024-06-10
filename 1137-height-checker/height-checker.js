932e350419b0912ad7ed6bae6ec79741a809ab06
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-06-10 06:35:20 +0530

    Time: 51 ms (75.26%) | Memory: 49 MB (54.04%) - LeetSync

---
+++ 1137-height-checker/height-checker.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
+var heightChecker = function(heights) {
+    let temp = new Array(...heights);
+    temp.sort((a,b)=>a-b);
+
+    let count = 0;
+    for(let i=0;i<heights.length;i++){
+        if(heights[i]!==temp[i])
+        count++;
+    }
+    return count;
+};
