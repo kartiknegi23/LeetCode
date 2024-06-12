fa9b90b8f5968be72b303449a43f5db6ecb6fadb
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-06-13 03:09:59 +0530

    Time: 64 ms (43.32%) | Memory: 53.8 MB (21.56%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.js
+++ 42-trapping-rain-water/trapping-rain-water.js
@@ -3,26 +3,24 @@
  * @return {number}
  */
 var trap = function(height) {
-    let left_max = new Array(height.length);
     let right_max = new Array(height.length);
+    let left_max = new Array(height.length);
 
-    let count = 0;
+    right_max[right_max.length-1] = height[height.length-1];
+    for(let i=height.length-2;i>=0;i--){
+        right_max[i] = Math.max(height[i], right_max[i+1]);
+    }
 
     left_max[0] = height[0];
     for(let i=1;i<height.length;i++){
-        left_max[i] = Math.max(left_max[i-1], height[i]);
+        left_max[i] = Math.max(height[i], left_max[i-1]);
     }
 
-    right_max[height.length-1] = height[height.length-1];
-
-    for(let i=height.length-2;i>=0;i--){
-        right_max[i] = Math.max(right_max[i+1], height[i]);
-    }
+    let count = 0;
 
     for(let i=0;i<height.length;i++){
-        count+=Math.min(left_max[i], right_max[i])-height[i];
+        count += Math.min(left_max[i], right_max[i]) - height[i];
     }
 
     return count;
-
 };
