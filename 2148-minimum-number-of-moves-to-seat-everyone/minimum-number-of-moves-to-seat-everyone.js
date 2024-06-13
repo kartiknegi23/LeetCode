80ebcc1f87b9d6c84c13d5c5f568aeb90b636fd4
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-06-13 12:21:14 +0530

    Time: 57 ms (67.51%) | Memory: 50.8 MB (47.21%) - LeetSync

---
+++ 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} seats
+ * @param {number[]} students
+ * @return {number}
+ */
+var minMovesToSeat = function(seats, students) {
+    let count = 0;
+    seats.sort((a,b)=>a-b);
+    students.sort((a,b)=>a-b);
+
+    for(let i=0;i<students.length;i++){
+        count+= Math.abs(students[i]-seats[i]);
+    }
+    return count;
+};
