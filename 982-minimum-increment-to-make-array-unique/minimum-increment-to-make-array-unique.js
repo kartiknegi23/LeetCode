24c2695443c905071efa04a4954d3ac473717ac2
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-06-14 06:49:00 +0530

    Time: 294 ms (5.09%) | Memory: 74.9 MB (5.09%) - LeetSync

---
+++ 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minIncrementForUnique = function(nums) {
+    let count = 0;
+    let hashmap = new Map();
+    let arr = new Array(nums.length);
+
+    nums.sort((a,b)=>a-b);
+
+    for(let i=0;i<nums.length;i++){
+        if(hashmap.has(nums[i])){
+            let sum = nums[i] + Math.abs(nums[i]-arr[i-1])+1;
+            arr[i] = sum;
+            hashmap.set(sum, 1);
+        }
+
+        else{
+            hashmap.set(nums[i], 1);
+            arr[i] = nums[i];
+        }
+    }
+
+    for(let i=0;i<nums.length;i++){
+        count+= arr[i]-nums[i];
+    }
+
+    return count;
+};
