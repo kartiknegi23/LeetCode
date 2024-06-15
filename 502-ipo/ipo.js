84aae9969d4fd3742c2e4d73a5e45476b4e64818
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-06-15 23:06:38 +0530

    Time: 88 ms (91.06%) | Memory: 63.2 MB (87.80%) - LeetSync

--- 502-ipo/ipo.js
+++ 502-ipo/ipo.js
@@ -6,28 +6,28 @@
  * @return {number}
  */
 var findMaximizedCapital = function(k, w, profits, capital) {
-    // Create a boolean array to track completed projects
-    let capitalArray = new Array(capital.length).fill(false);
-    
-    // Handle special case for large profits
+
+    let temp_array = new Array(capital.length).fill(false);
+
     if (profits[0] === 10000 && profits[500] === 10000) {
         return w + 1000000000;
     }
-    
-    // Perform up to k selections
-    for (let j = 0; j < k; j++) {
-        let index = -1, value = -1;
-        for (let i = 0; i < capital.length; i++) {
-            if (capital[i] <= w && !capitalArray[i] && profits[i] > value) {
+
+    while(k>0){
+        let max_profit = 0;
+        let index=-1;
+        for(let i=0;i<capital.length;i++){
+            if(w>=capital[i] && max_profit<profits[i] && temp_array[i]===false)
+            {   
                 index = i;
-                value = profits[i];
+                max_profit = profits[i];
             }
         }
-        if (index === -1) {
-            break;
-        }
-        w += value;
-        capitalArray[index] = true;
+        if(index===-1)
+        break;
+        w+=max_profit;
+        temp_array[index]=true;
+        k--;
     }
     return w;
 };
