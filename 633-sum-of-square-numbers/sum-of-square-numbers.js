e06e10ea987f3d13b8cae22b80d5b7cbf1c361fd
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-06-17 14:16:42 +0530

    Time: 66 ms (29.45%) | Memory: 49.1 MB (83.56%) - LeetSync

--- 633-sum-of-square-numbers/sum-of-square-numbers.js
+++ 633-sum-of-square-numbers/sum-of-square-numbers.js
@@ -5,13 +5,29 @@
 var judgeSquareSum = function(c) {
     let low = 0;
     let high = Math.floor(Math.sqrt(c));
+    let root = 0;
 
-    console.log(high);
+    // while(low<=high){
+    //     let mid = Math.floor((low+high)/2);
+    //     let product = mid*mid;
+
+    //     if(product===c)
+    //     return true;
+
+    //     else if(product<c){
+    //         root = mid;
+    //         low=mid+1;
+    //     }
+
+    //     else
+    //     high = mid-1;
+    // }
+
+    // low = 0;
+    // high = root;
 
     while(low<=high){
-        let sum = Math.pow(low,2) + Math.pow(high,2); 
-
-        console.log(sum, c);
+        let sum = (low*low) + (high*high); 
 
         if(sum == c)
         return true;
