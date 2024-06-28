97bf45af753dd51e365e89a0f12b10fe071fdeb1
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-06-28 06:30:17 +0530

    Time: 647 ms (5.95%) | Memory: 71.7 MB (14.06%) - LeetSync

---
+++ 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.java
@@ -0,0 +1,35 @@
+class Solution {
+    public long maximumImportance(int n, int[][] roads) {
+        Map<Integer, Integer> hashmap = new TreeMap<>();
+        int[] arr = new int[n];
+        long result = 0;
+
+        for(int i=0;i<roads.length;i++){
+            for(int j=0;j<2;j++){
+                if(hashmap.containsKey(roads[i][j]))
+                hashmap.put(roads[i][j], hashmap.get(roads[i][j])+1);
+
+                else
+                hashmap.put(roads[i][j], 1);
+            }
+        }
+
+        System.out.println(hashmap);
+
+        AtomicInteger node = new AtomicInteger(n);
+        hashmap.entrySet().stream()
+                .sorted(Map.Entry.<Integer, Integer>comparingByValue().reversed())
+                .forEach(entry -> {
+                    System.out.println("Key: " + entry.getKey() + ", Frequency: " + entry.getValue());
+                    arr[entry.getKey()] = node.getAndDecrement();
+                    });
+
+        System.out.println(Arrays.toString(arr));
+
+        for(int i=0;i<roads.length;i++){
+            result+= arr[roads[i][0]] + arr[roads[i][1]];
+        }
+
+        return result;
+    }
+}
