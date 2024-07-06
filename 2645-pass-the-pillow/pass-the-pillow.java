32608bd951ea880815601497a04566be6e4b6e8b
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-07-06 23:58:56 +0530

    Time: 0 ms (100.00%) | Memory: 40 MB (86.72%) - LeetSync

--- 2645-pass-the-pillow/pass-the-pillow.java
+++ 2645-pass-the-pillow/pass-the-pillow.java
@@ -3,31 +3,13 @@ class Solution {
         if(time<n)
         return time+1;
 
-        boolean front = true;
-        int result=0;
-        int count = 0;
-        while(count<time){
-            if(front==true)
-            {
-                for(int i=2;i<=n;i++){
-                    count++;
-                    result=i;
-                    if(count>=time)
-                    return result;
-                }
-                front=false;
-            }
+        int rounds = time/(n-1);
+        int remaining = time%(n-1);
 
-            else{
-                for(int i=n-1;i>=1;i--){
-                    result = i;
-                    count++;
-                    if(count>=time)
-                    return result;
-                }
-                front=true;
-            }
-        }
-        return result;
+        if(rounds%2==1)
+        return n-remaining;
+
+        else
+        return remaining+1;
     }
 }
