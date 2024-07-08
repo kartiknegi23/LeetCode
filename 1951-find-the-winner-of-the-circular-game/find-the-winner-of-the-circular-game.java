6b4808a1faea3fa1a5b5532696fc9237fbdbabb0
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-07-08 19:57:38 +0530

    Time: 79 ms (5.01%) | Memory: 41.9 MB (25.64%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
@@ -2,24 +2,39 @@ class Solution {
     public int findTheWinner(int n, int k) {
 
         List<Integer>list = new ArrayList<>();
+        int no_delete = 0;
         for(int i=1;i<=n;i++){
             list.add(i);
         }
 
             int count = 0;
             for(int i=0;i<list.size();i++){
+                if(list.get(i)==0 && i==(list.size()-1) && no_delete<(n-1)){
+                    i=-1;
+                    continue;
+                }
+                
+                
+                else if(list.get(i)==0)
+                continue;
                 count++;
-                if(count==k && list.size()>=2){
-                    list.remove((int) i);
+                if(count==k && no_delete<(n-1)){
+                    list.set((int) i, 0);
+                    no_delete++;
                     count=0;
-                    i--;
                 }
 
-                if(list.size()>=2 && i==(list.size()-1))
+                if(no_delete<(n-1) && i==(list.size()-1))
                 i=-1;
             }
         
+        System.out.println(list);
 
-        return list.get((int) 0);
+
+        for(int i=0;i<list.size();i++){
+            if(list.get(i)!=0)
+            return list.get(i);
+        }
+        return -1;
     }
 }
