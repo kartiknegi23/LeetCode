3ff20ca07deaece05cc8301c6bc384fdd4c5a5ad
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-07-11 17:42:23 +0530

    Time: 255 ms (5.11%) | Memory: 46.4 MB (5.53%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
@@ -1,15 +1,27 @@
 class Solution {
     public String reverseVowels(String s) {
         Stack<Character> stack = new Stack<>();
+        Set<Character> set= new HashSet<>();
+        set.add('a');
+        set.add('e');
+        set.add('i');
+        set.add('o');
+        set.add('u');
+        set.add('A');
+        set.add('E');
+        set.add('I');
+        set.add('O');
+        set.add('U');
+
         String result = "";
 
         for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='a' || s.charAt(i)=='e' || s.charAt(i)=='i' || s.charAt(i)=='o' || s.charAt(i)=='u' || s.charAt(i)=='A' || s.charAt(i)=='E' || s.charAt(i)=='I' || s.charAt(i)=='O' || s.charAt(i)=='U')
+            if(set.contains(s.charAt(i)))
             stack.push(s.charAt(i));
         }
 
         for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='a' || s.charAt(i)=='e' || s.charAt(i)=='i' || s.charAt(i)=='o' || s.charAt(i)=='u' || s.charAt(i)=='A' || s.charAt(i)=='E' || s.charAt(i)=='I' || s.charAt(i)=='O' || s.charAt(i)=='U')
+            if(set.contains(s.charAt(i)))
             result+=stack.pop();
 
             else
