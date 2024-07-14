c6c4d15ff9a8db9f07108dd34b9e0442d94da8c1
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-07-15 04:51:11 +0530

    Time: 5 ms (57.43%) | Memory: 41.7 MB (46.40%) - LeetSync

--- 726-number-of-atoms/number-of-atoms.java
+++ 726-number-of-atoms/number-of-atoms.java
@@ -6,24 +6,24 @@ class Solution {
         int i=0;
         int n=formula.length();
 
-        while(i<formula.length()){
-            if(formula.charAt(i)=='('){
+        while(i<formula.length()){  // Traverse the string //
+            if(formula.charAt(i)=='('){  // If we find '(' we make a new hashmap and it in the stack //
                 HashMap<String, Integer> newmap = new HashMap<>();
                 stack.push(newmap);
                 i++;
             }
 
-            else if(formula.charAt(i)==')'){
+            else if(formula.charAt(i)==')'){    // We pop the stack and get the elements from the top hashmap and merge it with the hashmap below it //
                 HashMap<String, Integer> currentmap = stack.pop();
                 i++;
 
-                StringBuilder multiplier = new StringBuilder();
+                StringBuilder multiplier = new StringBuilder();     // We check if there is any multiplier outise the bracket //
                 while(i<n && Character.isDigit(formula.charAt(i))){
                     multiplier.append(formula.charAt(i));
                     i++;
                 } 
 
-                if(multiplier.length()!=0){
+                if(multiplier.length()!=0){     //If there is a multiplier, we multiply the current element's frequency by that multiplier //
                     for(Map.Entry<String, Integer> entry : currentmap.entrySet()){
                         int number_multiplier = Integer.parseInt(multiplier.toString());
                         String key = entry.getKey();
@@ -36,7 +36,7 @@ class Solution {
 
                 
 
-                for(Map.Entry<String, Integer> entry : currentmap.entrySet()){
+                for(Map.Entry<String, Integer> entry : currentmap.entrySet()){      // Merge the popped hashmap with the current hashmap at the top of stack //
                     String key = entry.getKey();
                     int value = entry.getValue();
 
@@ -45,30 +45,30 @@ class Solution {
 
             }
 
-            else{
+            else{       // If only the element name is at this point //
                 StringBuilder element = new StringBuilder();
                 element.append(formula.charAt(i));
                 i++;
 
-                while(i<n && Character.isLetter(formula.charAt(i)) && Character.isLowerCase(formula.charAt(i))){
+                while(i<n && Character.isLetter(formula.charAt(i)) && Character.isLowerCase(formula.charAt(i))){        //We check if there is any suffix to the element symbol like a lowercase character //
                     element.append(formula.charAt(i));
                     i++;
                 }
 
                 StringBuilder frequency = new StringBuilder();
                 
-                while(i<n && Character.isDigit(formula.charAt(i))){
+                while(i<n && Character.isDigit(formula.charAt(i))){     //Check the no. of atoms of that element ie. a number to it's right //
                     frequency.append(formula.charAt(i));
                     i++;
                 }
 
                 int number_frequency = frequency.length()==0 ? 1 : Integer.parseInt(frequency.toString());
 
-                stack.peek().put(element.toString(), stack.peek().getOrDefault(element.toString(), 0)+number_frequency);
+                stack.peek().put(element.toString(), stack.peek().getOrDefault(element.toString(), 0)+number_frequency);    //Update the frequency in the top hashmap of stack //
             }
         }
 
-        TreeMap<String, Integer> treemap = new TreeMap<>();
+        TreeMap<String, Integer> treemap = new TreeMap<>();     //Use treemap to sort the elements by name //
 
         for(Map.Entry<String, Integer> entry : stack.peek().entrySet()){
             String key = entry.getKey();
@@ -79,7 +79,7 @@ class Solution {
 
         StringBuilder result = new StringBuilder();
 
-        for(Map.Entry<String, Integer> entry : treemap.entrySet()){
+        for(Map.Entry<String, Integer> entry : treemap.entrySet()){     // Store the element names and frequency in sorted order in a stringbuilder//
             String key = entry.getKey();
             int value = entry.getValue();
             result.append(key);
