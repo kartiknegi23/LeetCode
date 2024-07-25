ffbe30a69732b0f8c7a1d5ea53f633779bfe9d76
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-07-25 07:37:33 +0530

    Time: 772 ms (10.35%) | Memory: 56 MB (53.26%) - LeetSync

---
+++ 948-sort-an-array/sort-an-array.java
@@ -0,0 +1,57 @@
+class Solution {
+
+    public void mergesort(int[] nums, int low, int high) {
+        if (low == high)
+            return;
+
+        int mid = (int) Math.floor((low + high) / 2);
+
+        mergesort(nums, low, mid);
+        mergesort(nums, mid + 1, high);
+
+        merge(nums, low, mid, high);
+    }
+
+    public void merge(int[] nums, int low, int mid, int high) {
+        int[] temp = new int[high + 1];
+        int i = 0;
+        int left = low;
+        int right = mid + 1;
+
+        while (left <= mid && right <= high) {
+            if (nums[left] <= nums[right]) {
+                temp[i] = nums[left];
+                left++;
+                i++;
+            }
+
+            else {
+                temp[i] = nums[right];
+                right++;
+                i++;
+            }
+        }
+
+        while (left <= mid) {
+            temp[i] = nums[left];
+            left++;
+            i++;
+        }
+
+        while (right <= high) {
+            temp[i] = nums[right];
+            right++;
+            i++;
+        }
+
+        for (int j = low; j <= high; j++) {
+            nums[j] = temp[j - low];
+        }
+
+    }
+
+    public int[] sortArray(int[] nums) {
+        mergesort(nums, 0, nums.length - 1);
+        return nums;
+    }
+}
