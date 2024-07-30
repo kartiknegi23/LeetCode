8810e150e9723a0e64c44bf1f1d61b0e2e2d521b
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-07-31 00:57:50 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (71.78%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int fibo(int n, int[] dp){
+        if(n<=1){
+            dp[n]=n;
+            return dp[n];
+        }
+
+        if(dp[n]!=-1)
+        return dp[n];
+
+        return dp[n]=fibo(n-1, dp)+fibo(n-2, dp);
+    }
+
+    public int fib(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+
+        return fibo(n, dp);
+    }
+}
