6e4ab1efc47baa94ac134d983cbf1436964043b1
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-07-31 14:55:33 +0530

    Time: 13 ms (5.77%) | Memory: 55.6 MB (8.51%) - LeetSync

---
+++ 1196-filling-bookcase-shelves/filling-bookcase-shelves.java
@@ -0,0 +1,41 @@
+class Solution {
+    int WIDTH;
+    int[][] dp = new int[1000][1001];
+
+    public int solve(int[][] books, int i, int remaining, int maxheight){
+        if(i>=books.length){
+            return maxheight;
+        }
+
+        if(dp[i][remaining]!=-1)
+        return dp[i][remaining];
+
+        int height = books[i][1];
+        int width = books[i][0];
+
+        int keep = Integer.MAX_VALUE;
+        int skip = Integer.MIN_VALUE;
+
+        //Keep//
+        if(width<=remaining){
+            keep = solve(books, i+1, remaining-width, Math.max(maxheight, height));
+        }
+
+        //Skip//
+        skip = maxheight + solve(books, i+1, WIDTH-width, height);
+
+        return dp[i][remaining]=Math.min(keep,skip);
+
+    }
+
+    public int minHeightShelves(int[][] books, int shelfWidth) {
+        for (int i = 0; i < 1000; i++) {
+            for (int j = 0; j < 1001; j++) {
+                dp[i][j] = -1;
+            }
+        }
+
+        WIDTH = shelfWidth;
+        return solve(books, 0, shelfWidth, 0);
+    }
+}
