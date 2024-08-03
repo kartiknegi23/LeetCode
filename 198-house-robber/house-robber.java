71396d5ee50de227570ccdd83c3d38dcfb82d9ba
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-08-04 02:10:57 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (28.42%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,27 +1,27 @@
 class Solution {
     public int calc(int[] nums, int n, int[] dp){
-        if(n>=nums.length)
+        if(n==0)
+        return nums[n];
+
+        if(n<0)
         return 0;
 
         if(dp[n]!=-1)
         return dp[n];
 
-        int right = Integer.MIN_VALUE; 
-        
-        int left = calc(nums, n+1, dp);
+        //take//
+        int take = nums[n] + calc(nums, n-2, dp);
 
-        right = calc(nums, n+2, dp) + nums[n];
+        //skip//
+        int skip = calc(nums, n-1, dp);
 
-        return dp[n] = Math.max(left, right);
+        return dp[n] = Math.max(take, skip);
     }
 
     public int rob(int[] nums) {
-        if(nums.length==1)
-        return nums[0];
-
         int[] dp = new int[nums.length];
-        Arrays.fill(dp, -1);
+        Arrays.fill(dp , -1);
 
-        return calc(nums, 0, dp);        
+        return calc(nums, nums.length-1, dp);
     }
 }
