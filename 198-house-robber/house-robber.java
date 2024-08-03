8b4d584fad834a58357557ed5c3f3bb26ceca2ea
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-08-04 02:29:09 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (53.99%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,23 +1,5 @@
 class Solution {
-    public int calc(int[] nums, int n, int[] dp){
-        if(n==0)
-        return nums[n];
-
-        if(n<0)
-        return 0;
-
-        if(dp[n]!=-1)
-        return dp[n];
-
-        //take//
-        int take = nums[n] + calc(nums, n-2, dp);
-
-        //skip//
-        int skip = calc(nums, n-1, dp);
-
-        return dp[n] = Math.max(take, skip);
-    }
-
+    
     public int rob(int[] nums) {
         int[] dp = new int[nums.length];
         Arrays.fill(dp , -1);
