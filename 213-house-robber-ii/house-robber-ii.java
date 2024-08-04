c52547402b5da7b62bb4f20810dff8884c1dfc4b
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-08-04 18:02:55 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (15.01%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,24 +1,5 @@
 class Solution {
-    public int calc(int[] nums, int index, int start, int[] dp){
-        if(index==start)
-        return nums[index];
-
-        if(index<start)
-        return 0;
-
-        if(dp[index]!=-1)
-        return dp[index];
-
-        //Take//
-        int take = nums[index] + calc(nums, index-2, start, dp);
-
-        //Skip//
-        int skip = calc(nums, index-1, start, dp);
-
-        return dp[index] = Math.max(take, skip);
-    }
-
-
+    
     public int rob(int[] nums) {
         if(nums.length==1)
         return nums[0];
@@ -29,11 +10,34 @@ class Solution {
         int[] dp = new int[nums.length];
         Arrays.fill(dp, -1);
 
-        int first = calc(nums, nums.length-2, 0, dp);
+        dp[0] = nums[0];
+
+        for(int i=1;i<nums.length-1;i++){
+            int take = nums[i];
+            if(i>1)
+            take+=dp[i-2];
+
+            int skip = dp[i-1];
+
+            dp[i] = Math.max(take, skip);
+        }
+
+        int first = dp[dp.length-2];
 
         Arrays.fill(dp, -1);
+        dp[1] = nums[1];
 
-        int last = calc(nums, nums.length-1, 1, dp);
+        for(int i=2;i<nums.length;i++){
+            int take = nums[i];
+            if(i>2)
+            take+=dp[i-2];
+
+            int skip = dp[i-1];
+
+            dp[i] = Math.max(take, skip);
+        }
+
+        int last = dp[dp.length-1];
 
         return Math.max(first, last);
     }
