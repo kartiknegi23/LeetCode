bda81df3f6d6d64900b573bbaf1ece5b35262ab7
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-08-04 18:18:24 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (93.89%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,16 +1,10 @@
 class Solution {
 
-    public int rob(int[] nums) {
-        if(nums.length==1)
-        return nums[0];
-
-        if(nums.length==2)
-        return Math.max(nums[0], nums[1]);
-
+    public int calculate(int[] nums) {
         int prev1 = nums[0];
-        int prev2 = 0; 
+        int prev2 = 0;
 
-        for(int i=1;i<nums.length-1;i++){
+        for(int i=1;i<nums.length;i++){
             int take = nums[i];
             if(i>1)
             take+=prev2;
@@ -18,29 +12,38 @@ class Solution {
             int skip = prev1;
 
             int current = Math.max(take, skip);
+
             prev2 = prev1;
             prev1 = current;
         }
 
-        int first = prev1;
+        return prev1;
+    }
 
-        prev1 = nums[1];
-        prev2 = 0;
+    public int rob(int[] nums) {
+        if(nums.length==1)
+        return nums[0];
 
-        for(int i=2;i<nums.length;i++){
-            int take = nums[i];
-            if(i>2)
-            take+=prev2;
+        int[] arr1 = new int[nums.length-1];
+        int[] arr2 = new int[nums.length-1];
+        int index1=0;
+        int index2=0;
 
-            int skip = prev1;
+        for(int i=0;i<nums.length;i++){
+            if(i!=0)
+            {
+                arr2[index2]=nums[i];
+                index2++;
+            }
 
-            int current = Math.max(take, skip);
-            prev2 = prev1;
-            prev1 = current;
+            if(i!=nums.length-1){
+                arr1[index1]=nums[i];
+                index1++;
+            }
         }
 
-        int last = prev1;
+        return Math.max(calculate(arr1), calculate(arr2));
 
-        return Math.max(first, last);
+        
     }
 }
