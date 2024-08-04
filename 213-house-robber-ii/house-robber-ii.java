1108ac1c902dc85672c710026d0306f4ca4b7ac4
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-08-04 18:08:55 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (15.01%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,5 +1,5 @@
 class Solution {
-    
+
     public int rob(int[] nums) {
         if(nums.length==1)
         return nums[0];
@@ -7,37 +7,39 @@ class Solution {
         if(nums.length==2)
         return Math.max(nums[0], nums[1]);
 
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp, -1);
-
-        dp[0] = nums[0];
+        int prev1 = nums[0];
+        int prev2 = 0; 
 
         for(int i=1;i<nums.length-1;i++){
             int take = nums[i];
             if(i>1)
-            take+=dp[i-2];
+            take+=prev2;
 
-            int skip = dp[i-1];
+            int skip = prev1;
 
-            dp[i] = Math.max(take, skip);
+            int current = Math.max(take, skip);
+            prev2 = prev1;
+            prev1 = current;
         }
 
-        int first = dp[dp.length-2];
+        int first = prev1;
 
-        Arrays.fill(dp, -1);
-        dp[1] = nums[1];
+        prev1 = nums[1];
+        prev2 = 0;
 
         for(int i=2;i<nums.length;i++){
             int take = nums[i];
             if(i>2)
-            take+=dp[i-2];
+            take+=prev2;
 
-            int skip = dp[i-1];
+            int skip = prev1;
 
-            dp[i] = Math.max(take, skip);
+            int current = Math.max(take, skip);
+            prev2 = prev1;
+            prev1 = current;
         }
 
-        int last = dp[dp.length-1];
+        int last = prev1;
 
         return Math.max(first, last);
     }
