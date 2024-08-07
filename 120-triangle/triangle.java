6b08487cee6b62621a383ef3bea0e5c6a9a17651
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-08-08 01:57:06 +0530

    Time: 4 ms (47.67%) | Memory: 44.1 MB (52.90%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -24,10 +24,20 @@ class Solution {
 
         int[][] dp = new int[m][n];
 
-        for(int i=0;i<m;i++){
-            Arrays.fill(dp[i], -1);
+        for(int i=0;i<n;i++){
+            dp[m-1][i] = triangle.get(m-1).get(i);
+        }
+
+        for(int i=n-2;i>=0;i--){
+            for(int j=i;j>=0;j--){
+                int down = triangle.get(i).get(j) + dp[i+1][j];
+
+                int next = triangle.get(i).get(j) + dp[i+1][j+1];
+
+                dp[i][j] = Math.min(down, next);
+            }
         }
          
-        return min_path(triangle, 0, 0, dp);
+        return dp[0][0];
     }
 }
