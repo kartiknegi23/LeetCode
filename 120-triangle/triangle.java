6bd4d482af9a30a87bde92b143fb38ad2c237646
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-08-08 01:57:28 +0530

    Time: 5 ms (33.04%) | Memory: 44.3 MB (27.17%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,22 +1,4 @@
 class Solution {
-    public int min_path(List<List<Integer>> triangle, int i, int j, int[][] dp){
-        if(i==triangle.size()-1){
-            return triangle.get(i).get(j);
-        }
-
-        if(dp[i][j]!=-1)
-        return dp[i][j];
-
-        int down = Integer.MAX_VALUE;
-
-        if(i<triangle.size()-1){
-            down = triangle.get(i).get(j) + min_path(triangle, i+1, j, dp);
-        }
-
-        int next = triangle.get(i).get(j) + min_path(triangle, i+1, j+1, dp);
-
-        return dp[i][j] = Math.min(down, next);
-    }
 
     public int minimumTotal(List<List<Integer>> triangle) {
         int m = triangle.size();
