a617fecc61aeacc9af70823c8b22e670f9783ded
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-08-09 00:33:36 +0530

    Time: 5 ms (72.36%) | Memory: 45.1 MB (61.25%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -5,31 +5,33 @@ class Solution {
         if(n==1)
         return matrix[0][0];
 
-        int[][] dp = new int[n][n];
+        int[] dp = new int[n];
 
         for(int i=0;i<n;i++){
-            dp[0][i] = matrix[0][i];
+            dp[i] = matrix[0][i];
         }
 
         for(int i=1;i<n;i++){
             int left = Integer.MAX_VALUE;
             int right = Integer.MAX_VALUE;
+            int[] current = new int[n];
             for(int j=0;j<n;j++){
                 if(j>0)
-                left = dp[i-1][j-1];
+                left = dp[j-1];
 
-                int center = dp[i-1][j];
+                int center = dp[j];
 
                 if(j<n-1)
-                right = dp[i-1][j+1];
+                right = dp[j+1];
 
-                dp[i][j] = matrix[i][j] + Math.min(left, Math.min(center, right));
+                current[j] = matrix[i][j] + Math.min(left, Math.min(center, right));
             }
+            dp = current;
         }
 
         int result = Integer.MAX_VALUE;
         for(int i=0;i<n;i++){
-            result = Math.min(result, dp[n-1][i]);
+            result = Math.min(result, dp[i]);
         }
 
         return result;
