54bd93d6bc6806966096655535beeff02bb30afd
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-08-13 15:29:28 +0530

    Time: 4 ms (46.33%) | Memory: 42.9 MB (85.22%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,31 +1,33 @@
-import java.util.*;
-
 class Solution {
-    public void find_combination(int[] candidates, int target, List<Integer> list, List<List<Integer>> parent, int index, int sum) {
-        if (sum > target) return;
 
-        if (sum == target) {
-            parent.add(new ArrayList<>(list));
-            return;
+    public void find_combination(int[] candidates, int target, List<Integer> list, List<List<Integer>> result, int index){
+        if(target<0)
+        return;
+
+        if(target==0)
+        {
+            result.add(new ArrayList<>(list));
+            return ;
         }
 
-        for (int i = index; i < candidates.length; i++) {
-            // Skip duplicates
-            if (i > index && candidates[i] == candidates[i - 1]) continue;
+        for(int i=index;i<candidates.length;i++){
+            if(i>index && candidates[i] == candidates[i-1])
+            continue;
 
             list.add(candidates[i]);
-            find_combination(candidates, target, list, parent, i + 1, sum + candidates[i]);
-            list.remove(list.size() - 1);
+            find_combination(candidates, target-candidates[i], list, result, i+1);
+
+            list.remove(list.size()-1);
         }
     }
 
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
-        List<Integer> list = new ArrayList<>();
-        List<List<Integer>> parent = new ArrayList<>();
+        List<Integer>list = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
 
-        find_combination(candidates, target, list, parent, 0, 0);
+        find_combination(candidates, target, list, result, 0);
 
-        return parent;
+        return result;
     }
 }
