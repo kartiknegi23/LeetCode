311b566c426a86d199641cea3cf33d1b0611685a
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-08-17 03:15:14 +0530

    Time: 27 ms (80.78%) | Memory: 45.1 MB (77.42%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -9,28 +9,28 @@ class Solution {
         if(sum%2!=0)
         return false;
 
-        int[][] dp = new int[nums.length][sum/2+1];
+        int[] dp = new int[sum/2+1];
+    
+        dp[0] = 1;
         
-        for(int i=0;i<nums.length;i++){
-            dp[i][0] = 1;
-        }
-
         if(nums[0]<=sum/2)
-        dp[0][nums[0]]=1;
+        dp[nums[0]]=1;
 
         for(int index=1;index<nums.length;index++){
+            int[] curr = new int[sum/2+1]; 
             for(int target1=1;target1<=sum/2;target1++){
                 
                 int take = 0;
                 if(target1>=nums[index])
-                take = dp[index-1][target1-nums[index]];
+                take = dp[target1-nums[index]];
 
-                int skip = dp[index-1][target1];
+                int skip = dp[target1];
 
-                dp[index][target1] = take | skip;
+                curr[target1] = take | skip;
             }
+            dp = curr;
         }
 
-        return dp[nums.length-1][sum/2]==1? true : false;
+        return dp[sum/2]==1? true : false;
     }
 }
