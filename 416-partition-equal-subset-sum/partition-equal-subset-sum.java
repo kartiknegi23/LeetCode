f2b68194a95160d022bc994066ca69101d8aac37
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-08-17 03:10:12 +0530

    Time: 129 ms (11.15%) | Memory: 57.7 MB (15.86%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -2,7 +2,7 @@ class Solution {
 
     public int find(int[] nums, int index, int target1, int[][] dp){
         if(index<=0){
-            if(target1==0)
+            if(target1==nums[0])
             return 1;
 
             else
@@ -18,7 +18,10 @@ class Solution {
         if(dp[index][target1]!=-1)
         return dp[index][target1];
 
-        int take = find(nums, index-1, target1-nums[index], dp);
+        int take = 0;
+
+        if(target1>=nums[index])
+        take = find(nums, index-1, target1-nums[index], dp);
 
         int skip = find(nums, index-1, target1, dp);
 
@@ -37,12 +40,27 @@ class Solution {
         return false;
 
         int[][] dp = new int[nums.length][sum/2+1];
+        
         for(int i=0;i<nums.length;i++){
-            Arrays.fill(dp[i], -1);
+            dp[i][0] = 1;
         }
 
-        int result = find(nums, nums.length-1, sum/2, dp);
+        if(nums[0]<=sum/2)
+        dp[0][nums[0]]=1;
 
-        return result==1 ? true : false;
+        for(int index=1;index<nums.length;index++){
+            for(int target1=1;target1<=sum/2;target1++){
+                
+                int take = 0;
+                if(target1>=nums[index])
+                take = find(nums, index-1, target1-nums[index], dp);
+
+                int skip = find(nums, index-1, target1, dp);
+
+                dp[index][target1] = take | skip;
+            }
+        }
+
+        return dp[nums.length-1][sum/2]==1? true : false;
     }
 }
