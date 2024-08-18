99c466d3233c1c7dacf8cbb9b4a77e673b711c4f
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-08-18 13:49:05 +0530

    Time: 2 ms (91.97%) | Memory: 41.8 MB (46.15%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.java
+++ 264-ugly-number-ii/ugly-number-ii.java
@@ -1,12 +1,12 @@
 class Solution {
     public int nthUglyNumber(int n) {
-        int[] arr = new int[1690];
+        int[] arr = new int[n];
         arr[0]=1;
         int i2=0;
         int i3=0;
         int i5=0; 
 
-        for(int i=1;i<1690;i++){
+        for(int i=1;i<n;i++){
             arr[i] = Math.min(arr[i2]*2, Math.min(arr[i3]*3, arr[i5]*5));
 
             if(arr[i]==arr[i2]*2)
