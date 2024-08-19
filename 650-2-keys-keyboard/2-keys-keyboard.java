6d639350101a8c0c4e7a01168b01710b822ce1be
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-08-20 01:31:33 +0530

    Time: 91 ms (5.05%) | Memory: 54.9 MB (13.15%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.java
+++ 650-2-keys-keyboard/2-keys-keyboard.java
@@ -1,24 +1,4 @@
 class Solution {
-
-    public int calculate(int count, int clipboard, int n, int[][] dp){
-        if(count==n)
-        return 0;
-
-        if(count>n)
-        return 1000000;
-
-        if(dp[count][clipboard]!=-1)
-        return dp[count][clipboard];
-
-        //Paste//
-        int paste = 1 + calculate(count+clipboard, clipboard, n, dp);
-
-        //Copy & Paste//
-        int copy_paste = 1 + 1 + calculate(count*2, count, n, dp);
-
-        return dp[count][clipboard] = Math.min(paste, copy_paste);
-    }
-
     public int minSteps(int n) {
         if(n==1)
         return 0;
