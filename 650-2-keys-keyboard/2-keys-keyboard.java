520c01fa12e1afb0695671f418a4e8a41809ca09
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-08-20 01:31:14 +0530

    Time: 89 ms (5.05%) | Memory: 55.6 MB (9.04%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.java
+++ 650-2-keys-keyboard/2-keys-keyboard.java
@@ -25,10 +25,35 @@ class Solution {
 
         int[][] dp = new int[n+1][n+1];
 
-        for(int i=0;i<n;i++){
-            Arrays.fill(dp[i], -1);
+        // Initialize the dp array with a large value, indicating unvisited states
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= n; j++) {
+                dp[i][j] = Integer.MAX_VALUE;
+            }
         }
 
-        return 1 + calculate(1, 1, n, dp);
+        for(int i=0;i<=n;i++){
+            dp[n][i]=0;
+        }
+
+        for(int count=n-1;count>=0;count--){
+            for(int clipboard=n-1;clipboard>=0;clipboard--){
+
+                int paste = Integer.MAX_VALUE;
+                int copy_paste = Integer.MAX_VALUE;
+
+                //Paste//
+                if(count+clipboard<=n && dp[count+clipboard][clipboard]!=Integer.MAX_VALUE)
+                paste = 1 + dp[count+clipboard][clipboard];
+
+                //Copy & Paste//
+                if(count*2<=n && dp[count*2][count]!=Integer.MAX_VALUE)
+                copy_paste = 1 + 1 + dp[count*2][count];
+
+                dp[count][clipboard] = Math.min(paste, copy_paste);
+            }
+        }
+
+        return dp[1][0];
     }
 }
