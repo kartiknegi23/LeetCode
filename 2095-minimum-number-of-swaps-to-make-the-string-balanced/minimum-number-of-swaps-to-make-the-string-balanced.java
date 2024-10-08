3785d05bb60dfa878f38ae7ed642feeba3c412b2
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-10-08 14:02:39 +0530

    Time: 19 ms (38.41%) | Memory: 54.1 MB (70.84%) - LeetSync

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.java
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.java
@@ -1,17 +1,12 @@
 class Solution {
     public int minSwaps(String s) {
         int count = 0;
-        Stack<Character>stack = new Stack<>();
         for(int i=0;i<s.length();i++){
-            if(stack.size()==0 && s.charAt(i)==']')
+            if(s.charAt(i)=='[')
             count++;
 
-            else if(s.charAt(i)==']'){
-                stack.pop();
-            }    
-
-            else
-            stack.push(s.charAt(i));
+            else if(s.charAt(i)==']' && count>0)
+            count--;
         }
 
         return (count+1)/2;
