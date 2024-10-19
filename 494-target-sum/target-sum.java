4ddf316338d66358b8526674628320f7e43bf384
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-10-19 23:17:00 +0530

    Time: 8 ms (68.00%) | Memory: 44.7 MB (29.90%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int solve(int index, int current, int target, int[] nums){
+    public int solve(int index, int current, int target, int[] nums, int[][] dp, int offset){
         if(index>=nums.length){
             if(current==target)
             return 1;
@@ -8,18 +8,29 @@ class Solution {
             return 0;
         }
 
+        if(dp[index][current+offset]!=-1)
+        return dp[index][current+offset];
+
         //+//
-        int plus = solve(index+1, current+nums[index], target, nums);
+        int plus = solve(index+1, current+nums[index], target, nums, dp, offset);
 
         //-//
-        int minus = solve(index+1, current-nums[index], target, nums);
+        int minus = solve(index+1, current-nums[index], target, nums, dp, offset);
 
-        return plus+minus;
+        return dp[index][current+offset] = plus+minus;
     }
 
     public int findTargetSumWays(int[] nums, int target) {
-        int[][] dp = new int[nums.length][];
+        int offset = 0;
+        for(int i : nums){
+            offset+=i;
+        }
 
-        return solve(0, 0, target, nums);
+        int[][] dp = new int[nums.length][(2*offset)+1];
+        for(int i=0;i<dp.length;i++){
+            Arrays.fill(dp[i], -1);
+        }
+
+        return solve(0, 0, target, nums, dp, offset);
     }
 }
