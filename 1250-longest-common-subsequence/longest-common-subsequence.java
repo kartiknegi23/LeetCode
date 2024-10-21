1b63dc38c6ed2331c3463dc5bdb3c237f60fd44e
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-10-21 16:19:20 +0530

    Time: 23 ms (41.01%) | Memory: 50.8 MB (57.25%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,12 +1,12 @@
 class Solution {
     public int solve(int i, int j, String text1, String text2, int[][] dp){
-        if(i<0 || j<0)
+        if(i==0 || j==0)
         return 0;
 
         if(dp[i][j]!=-1)
         return dp[i][j];
 
-        if(text1.charAt(i)==text2.charAt(j)){
+        if(text1.charAt(i-1)==text2.charAt(j-1)){
             return 1 + solve(i-1, j-1, text1, text2, dp);
         }
 
@@ -20,11 +20,41 @@ class Solution {
     }
 
     public int longestCommonSubsequence(String text1, String text2) {
-        int[][] dp = new int[text1.length()][text2.length()];
+        int[][] dp = new int[text1.length()+1][text2.length()+1];
         for(int[] arr : dp){
             Arrays.fill(arr, -1);
         }
 
-        return solve(text1.length()-1, text2.length()-1, text1, text2, dp);
+        for(int i=0;i<=text1.length();i++){
+            dp[i][0] = 0;
+        }
+
+        for(int j=0;j<=text2.length();j++){
+            dp[0][j] = 0;
+        }
+
+        for(int i=1;i<=text1.length();i++){
+            for(int j=1;j<=text2.length();j++){
+                
+                    if(text1.charAt(i-1)==text2.charAt(j-1)){
+                        dp[i][j] = 1 + dp[i-1][j-1];
+                    }
+
+
+                    else{
+                        //first//
+                        int first = dp[i-1][j];
+
+                        //second//
+                        int second = dp[i][j-1];
+
+                        dp[i][j] = Math.max(first, second);
+                    }
+                    
+                    
+                }
+            }
+
+            return dp[text1.length()][text2.length()];
     }
 }
