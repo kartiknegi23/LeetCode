ab0059e821aebfe843430beb68e11a5d32522bad
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-10-24 15:07:23 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (71.34%) - LeetSync

--- 988-flip-equivalent-binary-trees/flip-equivalent-binary-trees.java
+++ 988-flip-equivalent-binary-trees/flip-equivalent-binary-trees.java
@@ -14,31 +14,23 @@
  * }
  */
 class Solution {
-    public boolean check_flip(TreeNode root1, TreeNode root2){
+    public boolean flipEquiv(TreeNode root1, TreeNode root2) {
         if(root1 == null && root2 == null)
         return true;
 
         if(root1 == null || root2 == null)
         return false;
 
-        if(root1.val == root2.val){
-            boolean noflip_left = check_flip(root1.left, root2.left);
-            boolean noflip_right = check_flip(root1.right, root2.right);
+        if(root1.val == root2.val)
+        {
+            boolean noflip = flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right);
 
-            boolean noflip = noflip_left && noflip_right;
-
-            boolean flip_left = check_flip(root1.left, root2.right);
-            boolean flip_right = check_flip(root1.right, root2.left);
-            
-            boolean flip = flip_left && flip_right;
+            boolean flip = flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left);
 
             return noflip || flip;
         }
-        
-        return false;
-    }
 
-    public boolean flipEquiv(TreeNode root1, TreeNode root2) {
-        return check_flip(root1, root2);
+        else
+        return false;
     }
 }
