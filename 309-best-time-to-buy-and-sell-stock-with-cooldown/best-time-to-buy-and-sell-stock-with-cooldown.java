60113a40a099ae596a03d951c811431849330e44
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-10-30 23:52:40 +0530

    Time: 2 ms (16.25%) | Memory: 41.2 MB (94.97%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -18,11 +18,26 @@ class Solution {
     }
 
     public int maxProfit(int[] prices) {
-        int[][] dp = new int[prices.length][2];
-        for(int[] arr : dp){
-            Arrays.fill(arr, -1);
+        int[][] dp = new int[prices.length+1][2];
+
+        dp[prices.length][0] = 0;
+        dp[prices.length][1] = 0;
+
+        for(int index=prices.length-1;index>=0;index--){
+            for(int buy=0;buy<2;buy++){
+                int profit = 0;
+                //can buy//
+                if(buy==1){
+                    profit = Math.max(-prices[index]+dp[index+1][0], dp[index+1][1]);
+                }
+                else{
+                    int sellingprice = index+2 < (prices.length) ? dp[index+2][1] : 0;
+                    profit = Math.max(prices[index]+sellingprice, dp[index+1][0]);
+                }
+                dp[index][buy] = profit;
+            }
         }
 
-        return solve(0, prices, 1, dp);
+        return dp[0][1];
     }
 }
