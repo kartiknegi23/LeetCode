39100d9367468c8dd6b2ddb2a71f5c7dfec985b5
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2024-11-15 05:06:28 +0530

    Time: 11 ms (17.83%) | Memory: 54.5 MB (93.46%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
@@ -1,41 +1,33 @@
 class Solution {
-    public int solve(int i, int j, int[][] matrix, int[][] dp){
-        if(i>=matrix.length || j>=matrix[0].length || matrix[i][j]==0)
+    public int findSub(int[][] matrix, int i, int j, int[][] dp){
+        if(i<0 || i>=matrix.length || j<0 || j>=matrix[0].length || matrix[i][j]==0 )
         return 0;
-  
+
         if(dp[i][j]!=-1)
         return dp[i][j];
 
-        int right = 1 + solve(i, j+1, matrix, dp);
-        int down = 1 + solve(i+1, j, matrix, dp);
-        int diagonal = 1 + solve(i+1, j+1, matrix, dp);
+        int right = findSub(matrix, i, j+1, dp);
+        int down = findSub(matrix, i+1, j, dp);
+        int diagonal = findSub(matrix, i+1, j+1, dp);
 
-        return dp[i][j] = Math.min(right, Math.min(down, diagonal));
+        return dp[i][j] = 1+Math.min(right, Math.min(down, diagonal));
     }
 
     public int countSquares(int[][] matrix) {
-        int result = 0;
-        int[][] dp = new int[matrix.length+1][matrix[0].length+1];
+        int count = 0;
+
+        int[][] dp = new int[matrix.length][matrix[0].length];
+        for(int[] arr : dp){
+            Arrays.fill(arr, -1);
+        }
 
         for(int i=0;i<matrix.length;i++){
             for(int j=0;j<matrix[0].length;j++){
-                if(i==0 || j==0){
-                    dp[i][j] = matrix[i][j];
-                }
-
-                else if(matrix[i][j]==1)
-                {
-                    int left = 1 + dp[i][j-1];
-                    int up = 1 + dp[i-1][j];
-                    int diagonal = 1 + dp[i-1][j-1];
-
-                    dp[i][j] = Math.min(left, Math.min(up, diagonal));
-                }
-
-                result += dp[i][j];
+                if(matrix[i][j]==1)
+                count += findSub(matrix, i, j, dp);
             }
         }
-            
-        return result;
+
+        return count;
     }
 }
