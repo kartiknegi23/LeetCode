2c1e0c824cb2ed2208075da9cb9555bcc8d43083
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2025-01-12 01:14:02 +0530

    Time: 5 ms (58.44%) | Memory: 45.2 MB (74.68%) - LeetSync

---
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean canConstruct(String s, int k) {
+        if(s.length() < k)
+        return false;
+
+        if(s.length() == k)
+        return true;
+
+        int[] count = new int[26];
+        for(int i=0;i<s.length();i++){
+            count[s.charAt(i)-'a']++;
+        }
+
+        int odd_count = 0;
+        for(int i=0;i<26;i++){
+            if(count[i]%2 != 0)
+            odd_count++;
+        }
+
+        if(odd_count<=k)
+        return true;
+        
+        else
+        return false;
+
+    }
+}
