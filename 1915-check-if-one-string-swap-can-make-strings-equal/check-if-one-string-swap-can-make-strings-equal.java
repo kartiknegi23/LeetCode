d8985fddc29b8afa4f1b7bd853bc471334bcd17a
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2025-02-06 00:33:14 +0530

    Time: 1 ms (52.21%) | Memory: 41.2 MB (97.10%) - LeetSync

--- 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.java
+++ 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.java
@@ -3,29 +3,17 @@ class Solution {
         if(s1.equals(s2))
         return true;
 
-        int[] arr1 = new int[26];
-        int[] arr2 = new int[26];
-
-        for(int i=0;i<s1.length();i++){
-            arr1[s1.charAt(i)-'a']++;
-        }
-
-        for(int i=0;i<s2.length();i++){
-            arr2[s2.charAt(i)-'a']++;
-        }
-
-        for(int i=0;i<26;i++){
-            if(arr1[i] != arr2[i])
-            return false;
-        }
-        
         int i=0;
         int j=0;
         int count = 0;
+        List<Integer> list = new ArrayList<>();
         
         while(i<s1.length() && j<s2.length()){
             if(s1.charAt(i) != s2.charAt(j))
-            count++;
+            {
+                count++;
+                list.add(i);
+            }
 
             if(count>2)
             return false;
@@ -33,6 +21,29 @@ class Solution {
             i++;
             j++;
         }
+        
+        int counter = 0;
+
+        List<Character> l1 = new ArrayList<>();
+        List<Character> l2 = new ArrayList<>();
+
+        for(int k=0;k<list.size();k++){
+            l1.add(s1.charAt(list.get(k)));
+            l2.add(s2.charAt(list.get(k)));
+        }
+        
+        for(int k=0;k<l1.size();k++){
+            boolean find = false;
+            for(int l=0;l<l2.size();l++){
+                if(l1.get(k) == l2.get(l)){
+                    find = true;
+                    break;
+                }
+            }
+            if(find == false)
+            return false;
+        }
+
         return true;
     }
 }
