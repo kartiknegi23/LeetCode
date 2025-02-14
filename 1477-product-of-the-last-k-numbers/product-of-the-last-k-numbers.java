2aad93502d8d4411bf33d9a910d93fd410a1896f
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2025-02-14 21:59:43 +0530

    Time: 17 ms (33.70%) | Memory: 68.8 MB (65.04%) - LeetSync

--- 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.java
+++ 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.java
@@ -1,22 +1,34 @@
 class ProductOfNumbers {
     List<Integer> list;
+    List<Integer> prefix;
     public ProductOfNumbers() {
         list = new ArrayList<>();
+        prefix = new ArrayList<>();
     }
     
     public void add(int num) {
+        int n = prefix.size();
         list.add(num);
+        if(n==0){
+            prefix.add(num);
+        }   
+        else{
+            prefix.add(prefix.get(n-1)*num);
+        }
+        if(prefix.get(prefix.size()-1) == 0)
+        prefix.clear();
     }
     
     public int getProduct(int k) {
-        int right = list.size()-1;
-        int left = list.size()-k;
-        int product = 1;
-        while(right>=left){
-            product *= list.get(right);
-            right--;
-        }
-        return product;
+        int n = prefix.size();
+
+        if(n == 0 || k>n)
+        return 0;
+
+        if(k==n)
+        return prefix.get(n-1);
+
+        return prefix.get(n-1)/prefix.get(n-k-1);
     }
 }
 
