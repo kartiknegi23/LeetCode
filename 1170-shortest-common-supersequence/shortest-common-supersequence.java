9443bbeefb025862c4602078cfaf905df326be62
Kartik Negi <77157303+kartiknegi23@users.noreply.github.com>
2025-02-28 22:55:35 +0530

    Time: 24 ms (39.66%) | Memory: 50.3 MB (76.22%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.java
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.java
@@ -1,58 +1,53 @@
 class Solution {
     public String shortestCommonSupersequence(String str1, String str2) {
         int[][] dp = new int[str1.length()+1][str2.length()+1];
-
-        for(int i=0;i<=str1.length();i++){
-            dp[i][0] = 0;
-        }
-
-        for(int i=0;i<=str2.length();i++){
-            dp[0][i] = 0;
-        }
-
+        
         for(int i=1;i<=str1.length();i++){
             for(int j=1;j<=str2.length();j++){
                 if(str1.charAt(i-1) == str2.charAt(j-1)){
                     dp[i][j] = 1 + dp[i-1][j-1];
                 }
 
-                else
-                dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                else{
+                    int left = dp[i-1][j];
+                    int right = dp[i][j-1];
+                    dp[i][j] = Math.max(left, right);
+                }
             }
         }
 
         int i = str1.length();
         int j = str2.length();
-        StringBuilder ans = new StringBuilder();
+        StringBuilder result = new StringBuilder();
 
         while(i>0 && j>0){
             if(str1.charAt(i-1) == str2.charAt(j-1)){
-                ans.append(str1.charAt(i-1));
+                result.append(str1.charAt(i-1));
                 i--;
                 j--;
             }
 
-            else if(dp[i-1][j] > dp[i][j-1]){
-                ans.append(str1.charAt(i-1));
-                i--;
+            else if(dp[i][j-1] > dp[i-1][j]){
+                result.append(str2.charAt(j-1));
+                j--;
             }
 
             else{
-                ans.append(str2.charAt(j-1));
-                j--;
+                result.append(str1.charAt(i-1));
+                i--;
             }
         }
 
         while(i>0){
-            ans.append(str1.charAt(i-1));
+            result.append(str1.charAt(i-1));
             i--;
         }
 
         while(j>0){
-            ans.append(str2.charAt(j-1));
+            result.append(str2.charAt(j-1));
             j--;
         }
 
-        return ans.reverse().toString();
+        return result.reverse().toString();
     }
 }
